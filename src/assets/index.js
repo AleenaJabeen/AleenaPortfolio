bd81d31d79ecac3d402932c0f181bf5212556369
AleenaJabeen <aleenajabeen19648@gmail.com>
2026-04-21 12:35:06 +0500

    asset issue resolved

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import devloom from './Projects/Devloom.png';
+import profile from './Projects/picture.png';
+import about from './Projects/about2.jpeg';
+
+
+export {devloom,profile,about,about1,profile1}
